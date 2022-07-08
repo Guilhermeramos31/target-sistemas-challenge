78a647eb0ea15c353f99224f746141a165062a4e
Guilherme Ramos <guilhermeramos31.gr@gmail.com>
2022-07-08 01:29:59 -0300

    feat-fibonacci

--- fibonacci/app.js
+++ fibonacci/app.js
@@ -1,14 +1,14 @@
-console.log(fibonacci(610));
+console.log(fibonacci("1"));
 
 function fibonacci (num){
     let number = [0,1];
     for (let i = 0; i < number.length; i++) {
         var result = number[i]+number[i+1];
-        number.push(result)
-        if(result === num) {
-            return ('O numero pertence a sequênciade Fibonacci.')
-        }  else if(result > num) {
-                return ('O numero não pertence a sequênciade Fibonacci.');
+        number.push(result);
+        if(result == num || num == 0) {
+            return ('O numero pertence a sequênciade Fibonacci.');
+        }  else if(result > num || typeof num === "string") {
+            return ('O numero não pertence a sequênciade Fibonacci.');
         }
     }
 }
