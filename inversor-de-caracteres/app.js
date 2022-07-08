7a1e9ae9f6f2382a1d5244c2eaebe9b52718c034
Guilherme Ramos <guilhermeramos31.gr@gmail.com>
2022-07-08 01:48:49 -0300

    feat-inversor-de-caracteres

--- inversor-de-caracteres/app.js
+++ inversor-de-caracteres/app.js
@@ -1,1 +1,11 @@
+const string = "target sistemas";
 
+console.log(reverse(string));
+
+function reverse(a) {
+    var gnirts = [];
+    for (let i = a.length; i > 0; i--) {
+        gnirts.push(a[i-1]);
+    }
+    return gnirts.toString().replace(/[',']/g,'');
+}
